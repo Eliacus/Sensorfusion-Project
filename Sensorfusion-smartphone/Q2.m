e767f9e82fbc94fde108283d3759b1dbc89760c5
Eliacus <eliasneme2@gmail.com>
2019-05-29 14:19:35 +0200

    mean and covariances in Q2

--- Sensorfusion-smartphone/Q2.m
+++ Sensorfusion-smartphone/Q2.m
@@ -1,9 +1,28 @@
 %% Q2
-%Get the data from the phone:
-%[xhat, meas] = filterTemplate(); % save data
+% Get the data from the phone:
+%   [xhat, meas] = filterTemplate(); % save data
 %%
-clc;clear all;close all;
+clear; clc; close all;
+
 %Load data:
-load('Q2_data.mat')
+load('Q2_data.mat') 
+
+% -- Calculate means and covariances --
+
+% Accelerometer
+mean_acc = mean(meas.acc(:,~any(isnan(meas.acc),1)),2);
+cov_acc = cov(meas.acc(:,~any(isnan(meas.acc),1))');
+
+% Gyroscope
+mean_gyr = mean(meas.gyr(:,~any(isnan(meas.gyr),1)),2);
+cov_gyr = cov(meas.gyr(:,~any(isnan(meas.gyr),1))');
+
+% Magnetometer
+mean_mag = mean(meas.mag(:,~any(isnan(meas.mag),1)),2);
+cov_mag = cov(meas.mag(:,~any(isnan(meas.mag),1))');
+
+
+
+
 
 
