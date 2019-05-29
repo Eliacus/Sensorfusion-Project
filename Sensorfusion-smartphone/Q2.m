39db01e959efc4ddc3a87fb9273a941524832571
AdmirA <admir46@hotmail.com>
2019-05-29 14:03:51 +0200

    Q2 eld

--- Sensorfusion-smartphone/Q2.m
+++ Sensorfusion-smartphone/Q2.m
@@ -1,1 +1,9 @@
-[xhat, meas] = filterTemplate(); % save data
+%% Q2
+%Get the data from the phone:
+%[xhat, meas] = filterTemplate(); % save data
+%%
+clc;clear all;close all;
+%Load data:
+load('Q2_data.mat')
+
+
